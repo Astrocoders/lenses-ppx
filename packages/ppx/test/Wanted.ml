29ec2653ac76e6f119cbb21a49a4ce342adf0351
Kevin COMBRIAT <kevin@sc0ville.com>
2019-09-28 11:43:34 +0900

    fix(test): Wanted ml file

--- packages/ppx/test/Wanted.ml
+++ packages/ppx/test/Wanted.ml
@@ -1,35 +1,48 @@
-module User =
+module FormConfig =
   struct
-    type t = {
-      email: string;
-      age: int;
-      hobbies: string array;}
-    type _ field =
-      | Email: string field
-      | Age: int field
-      | Hobbies: int -> string option field
-    let get : 'value . t -> 'value field -> 'value= fun (type value) ->
-      (fun t  ->
-         fun field  ->
-           match field with
-           | Email  -> t.email
-           | Age  -> t.age
-           | Hobbies index ->
-               (try Some ((t.hobbies).(index)) with | _ -> None) : t ->
-                                                                    value
-                                                                    field ->
-                                                                    value)
-    let set : 'value . t -> 'value field -> 'value -> t= fun (type value) ->
-      (fun t  ->
-         fun field  ->
-           fun value  ->
-             match field with
-             | Email  -> { t with email = value }
-             | Age  -> { t with age = value }
-             | Hobbies index ->
-                 (match value with
-                  | Some value ->
-                      ((t.hobbies).(index) <- value;
-                       { t with hobbies = (t.hobbies) })
-                  | None  -> hobbies) : t -> value field -> value -> t)
-end
+    module State =
+      struct
+        type t = {
+          email: string;
+          age: int;
+          hobbies: string array;}
+        type _ field =
+          | Email: string field
+          | Age: int field
+          | Hobbies: string array field
+          | HobbiesAt: int -> string option field
+          | HobbiesAtExn: int -> string field
+        let get : 'value . t -> 'value field -> 'value= fun (type value) ->
+          (fun t  ->
+             fun field  ->
+               match field with
+               | Email  -> t.email
+               | Age  -> t.age
+               | Hobbies  -> t.hobbies
+               | HobbiesAt index ->
+                   (try Some ((t.hobbies).(index)) with | _ -> None)
+               | HobbiesAtExn index -> (t.hobbies).(index) : t ->
+                                                               value field ->
+                                                                 value)
+        let set : 'value . t -> 'value field -> 'value -> t= fun (type value)
+          ->
+          (fun t  ->
+             fun field  ->
+               fun value  ->
+                 match field with
+                 | Email  -> { t with email = value }
+                 | Age  -> { t with age = value }
+                 | Hobbies  -> { t with hobbies = value }
+                 | HobbiesAt index ->
+                     (match value with
+                      | Some value ->
+                          ((t.hobbies).(index) <- value;
+                           { t with hobbies = (t.hobbies) })
+                      | None  -> t)
+                 | HobbiesAtExn index ->
+                     ((t.hobbies).(index) <- value;
+                      { t with hobbies = (t.hobbies) }) : t ->
+                                                            value field ->
+                                                              value -> t)
+      end
+  end
