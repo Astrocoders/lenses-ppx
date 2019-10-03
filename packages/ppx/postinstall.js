6d62ffde713e530f38f391271c950086fcec266a
Roman Schejbal <schejbalroman@gmail.com>
2019-10-03 16:48:32 +0200

    Use process.platform

--- packages/ppx/postinstall.js
+++ packages/ppx/postinstall.js
@@ -1,18 +1,14 @@
-const { exec } = require("child_process");
 const fs = require("fs");
 
-exec("uname", (error, stdout, stderr) => {
-  if (error) {
-    // probably windows, uname does not exist
-    fs.copyFileSync("windows", "ppx.exe"); // copy as windows needs two ppx files for some reason, one extra with exe suffix
-    fs.chmodSync("ppx.exe", 0o744);
-    fs.renameSync("windows", "ppx");
-  } else {
-    const platform = stdout.trim().toLowerCase();
-    if (!fs.existsSync(platform)) {
-      throw new Error(platform + " lenses-ppx binary not found");
-    }
-    fs.renameSync(platform, "ppx");
-  }
-  fs.chmodSync("ppx", 0o744);
-});
+const { platform } = process;
+if (!fs.existsSync(platform)) {
+  throw new Error(platform + " lenses-ppx binary not found");
+}
+
+if (platform === "win32") {
+  fs.copyFileSync("win32", "ppx.exe"); // copy as windows needs two ppx files for some reason, one extra with exe suffix
+  fs.chmodSync("ppx.exe", 0o744);
+}
+
+fs.renameSync(platform, "ppx");
+fs.chmodSync("ppx", 0o744);
